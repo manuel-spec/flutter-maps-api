31767e1c44482ea2e081bdf52eaf6d48a0836075
manuel-spec <amaniase084@gmail.com>
2024-05-31 04:14:25 -0700

    pinning markers

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Home')), body: content()));
+    return MaterialApp(home: Scaffold(body: content()));
   }
 
   Widget content() {
     return FlutterMap(
-      children: [OpenStreetMapTileLayer],
+      children: [
+        OpenStreetMapTileLayer,
+        MarkerLayer(markers: [
+          Marker(
+            width: 80.0,
+            height: 80.0,
+            point: LatLng(double.parse('9.0192'), double.parse('38.7525')),
+            child: Icon(Icons.location_on, size: 40.0, color: Colors.red),
+          ),
+        ])
+      ],
       options: MapOptions(
-          initialCenter:
-              LatLng(double.parse('37.7749'), double.parse('-122.4194')),
-          initialZoom: 13.0,
-          interactionOptions:
-              InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom)),
+        initialCenter:
+            // 9.0192° N, 38.7525° E
+            LatLng(double.parse('9.0192'), double.parse('38.7525')),
+        initialZoom: 13.0,
+        interactionOptions:
+            InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
+      ),
     );
   }
 }
